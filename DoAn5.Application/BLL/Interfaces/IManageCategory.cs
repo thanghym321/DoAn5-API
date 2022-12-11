68a6d7f389f08c5c44e966fde5f235ce99fe5ac6
thanghym321 <72644880+thanghym321@users.noreply.github.com>
2022-12-11 16:45:20 +0700

    update 4h45 11/12/2022

---
+++ DoAn5.Application/BLL/Interfaces/IManageCategory.cs
@@ -0,0 +1,19 @@
+﻿using DoAn5.Application.Common;
+using DoAn5.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn5.Application.BLL.Interfaces
+{
+    public interface IManageCategory
+    {
+        Task<List<Category>> Get();
+        Task<PagedResult<Category>> GetAllPaging(int PageIndex, int PageSize, string keyword);
+        Task<Category> GetById(int Id);
+        Task<int> Create(Category request);
+        Task<int> Update(Category request);
+        Task<int> Delete(int Id);
+    }
+}
